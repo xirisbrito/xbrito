cbbfbaa04b638fe345c34541c0a31e139aa61d64
xbrito <dougclosed@hotmail.com>
2025-11-24 17:20:22 -0300

    deixando o jogo mais facil

--- Jogo-do-n-mero-secreto-main/app.js
+++ Jogo-do-n-mero-secreto-main/app.js
@@ -8,7 +8,10 @@ function exibirTextoNaTela(tag, texto) {
     campo.innerHTML = texto;
     responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
 }
-    exibirMensagemInicial();
+exibirTextoNaTela('h1', 'Jogo do número secreto');
+        exibirTextoNaTela('p', 'Escolha entre um número de 1 a 10');
+
+        exibirMensagemInicial();
 
         
 function exibirMensagemInicial() {
